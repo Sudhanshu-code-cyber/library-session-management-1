14f383079c306b88c3aca8b344e96f914739adbf
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2024-11-14 09:20:26 +0530

    adding session thoda complete

--- app/(front)/addingSession/page.js
+++ app/(front)/addingSession/page.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      this is addingsession
+    <div className='flex flex-col'>
+      <div className='flex text-4xl font-sans font-semibold bg-[#4A628A] text-[#DFF2Eb] shadow-b-xl justify-center py-5 '>
+        Add Session
+        </div>
+     
+       <form action='' className='flex justify-center p-5 ' >
+           <div className='flex w-[50%]  justify-center gap-5 '>
+           <label id="Sname" className='text-xl   font-semibold font-sans '>
+                Student Name:
+            </label>
+            <input id='Sname' className='p-1 w-[50%]  outline-none border-[#7AB2D3]'/>
+
+           </div>
+        </form>
+       
     </div>
   )
 }
