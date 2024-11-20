98ffd22e27d447bc8f0ed85dd136723692baca31
theaman82 <amankumaraman55555@gmail.com>
2024-11-20 12:08:04 +0530

    all fine

--- app/(front)/manage-checkout/page.js
+++ app/(front)/manage-checkout/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 const page = () => {
   return (
-    <div className='flex flex-col gap-5'>
-        <h1 className='text-2xl font-bold'>Checkout Sessions</h1>
+    <div className='flex  gap-5'>
+      <div className='w-3/12'><Sidebar/></div>
+       <div>
+          <h1 className='text-2xl mt-10 font-bold'>Checkout Sessions</h1>
+       </div>
         
     </div>
   )
